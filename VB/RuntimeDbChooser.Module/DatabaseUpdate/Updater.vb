5afcc455852ecd6ae2ac43db0a56c95c2dd00ae2
Andrey <freazeek@mail.ru>
2020-08-12 14:24:56 +0300

    use PermissionPolicy types in updater

--- VB/RuntimeDbChooser.Module/DatabaseUpdate/Updater.vb
+++ VB/RuntimeDbChooser.Module/DatabaseUpdate/Updater.vb
@@ -9,6 +9,7 @@ Imports DevExpress.ExpressApp.Security.Strategy
 Imports DevExpress.Xpo
 Imports DevExpress.ExpressApp.Xpo
 Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.BaseImpl.PermissionPolicy
 
 Namespace RuntimeDbChooser.Module.DatabaseUpdate
     ' For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppUpdatingModuleUpdatertopic.aspx
@@ -20,40 +21,40 @@ Namespace RuntimeDbChooser.Module.DatabaseUpdate
         End Sub
         Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
             MyBase.UpdateDatabaseAfterUpdateSchema()
-            Dim userAdmin As SecuritySystemUser = ObjectSpace.FindObject(Of SecuritySystemUser)(New BinaryOperator("UserName", "Admin"))
+            Dim userAdmin As PermissionPolicyUser = ObjectSpace.FindObject(Of PermissionPolicyUser)(New BinaryOperator("UserName", "Admin"))
             If userAdmin Is Nothing Then
-                userAdmin = ObjectSpace.CreateObject(Of SecuritySystemUser)()
+                userAdmin = ObjectSpace.CreateObject(Of PermissionPolicyUser)()
                 userAdmin.UserName = "Admin"
                 ' Set a password if the standard authentication type is used
                 userAdmin.SetPassword("")
             End If
             ' If a role with the Administrators name doesn't exist in the database, create this role
-            Dim adminRole As SecuritySystemRole = ObjectSpace.FindObject(Of SecuritySystemRole)(New BinaryOperator("Name", "Administrators"))
+            Dim adminRole As PermissionPolicyRole = ObjectSpace.FindObject(Of PermissionPolicyRole)(New BinaryOperator("Name", "Administrators"))
             If adminRole Is Nothing Then
-                adminRole = ObjectSpace.CreateObject(Of SecuritySystemRole)()
+                adminRole = ObjectSpace.CreateObject(Of PermissionPolicyRole)()
                 adminRole.Name = "Administrators"
             End If
             adminRole.IsAdministrative = True
             userAdmin.Roles.Add(adminRole)
 
             If ObjectSpace.Database.Contains("DB1") Then
-                Dim sampleUser1 As SecuritySystemUser = ObjectSpace.FindObject(Of SecuritySystemUser)(New BinaryOperator("UserName", "User1"))
+                Dim sampleUser1 As PermissionPolicyUser = ObjectSpace.FindObject(Of PermissionPolicyUser)(New BinaryOperator("UserName", "User1"))
                 If sampleUser1 Is Nothing Then
-                    sampleUser1 = ObjectSpace.CreateObject(Of SecuritySystemUser)()
+                    sampleUser1 = ObjectSpace.CreateObject(Of PermissionPolicyUser)()
                     sampleUser1.UserName = "User1"
                     sampleUser1.SetPassword("")
                 End If
-                Dim defaultRole As SecuritySystemRole = CreateDefaultRole()
+                Dim defaultRole As PermissionPolicyRole = CreateDefaultRole()
                 sampleUser1.Roles.Add(defaultRole)
             End If
             If ObjectSpace.Database.Contains("DB2") Then
-                Dim sampleUser2 As SecuritySystemUser = ObjectSpace.FindObject(Of SecuritySystemUser)(New BinaryOperator("UserName", "User2"))
+                Dim sampleUser2 As PermissionPolicyUser = ObjectSpace.FindObject(Of PermissionPolicyUser)(New BinaryOperator("UserName", "User2"))
                 If sampleUser2 Is Nothing Then
-                    sampleUser2 = ObjectSpace.CreateObject(Of SecuritySystemUser)()
+                    sampleUser2 = ObjectSpace.CreateObject(Of PermissionPolicyUser)()
                     sampleUser2.UserName = "User2"
                     sampleUser2.SetPassword("")
                 End If
-                Dim defaultRole As SecuritySystemRole = CreateDefaultRole()
+                Dim defaultRole As PermissionPolicyRole = CreateDefaultRole()
                 sampleUser2.Roles.Add(defaultRole)
             End If
             ObjectSpace.CommitChanges()
@@ -64,18 +65,22 @@ Namespace RuntimeDbChooser.Module.DatabaseUpdate
             '    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
             '}
         End Sub
-        Private Function CreateDefaultRole() As SecuritySystemRole
-            Dim defaultRole As SecuritySystemRole = ObjectSpace.FindObject(Of SecuritySystemRole)(New BinaryOperator("Name", "Default"))
+        Private Function CreateDefaultRole() As PermissionPolicyRole
+            Dim defaultRole As PermissionPolicyRole = ObjectSpace.FindObject(Of PermissionPolicyRole)(New BinaryOperator("Name", "Default"))
             If defaultRole Is Nothing Then
-                defaultRole = ObjectSpace.CreateObject(Of SecuritySystemRole)()
+                defaultRole = ObjectSpace.CreateObject(Of PermissionPolicyRole)()
                 defaultRole.Name = "Default"
 
-                defaultRole.AddObjectAccessPermission(Of SecuritySystemUser)("[Oid] = CurrentUserId()", SecurityOperations.ReadOnlyAccess)
-                defaultRole.AddMemberAccessPermission(Of SecuritySystemUser)("ChangePasswordOnFirstLogon", SecurityOperations.Write, "[Oid] = CurrentUserId()")
-                defaultRole.AddMemberAccessPermission(Of SecuritySystemUser)("StoredPassword", SecurityOperations.Write, "[Oid] = CurrentUserId()")
-                defaultRole.SetTypePermissionsRecursively(Of SecuritySystemRole)(SecurityOperations.Read, SecuritySystemModifier.Allow)
-                defaultRole.SetTypePermissionsRecursively(Of ModelDifference)(SecurityOperations.ReadWriteAccess, SecuritySystemModifier.Allow)
-                defaultRole.SetTypePermissionsRecursively(Of ModelDifferenceAspect)(SecurityOperations.ReadWriteAccess, SecuritySystemModifier.Allow)
+           
+			defaultRole.AddObjectPermission(Of PermissionPolicyUser)(SecurityOperations.Read, "[Oid] = CurrentUserId()", SecurityPermissionState.Allow)
+            defaultRole.AddNavigationPermission("Application/NavigationItems/Items/Default/Items/MyDetails", SecurityPermissionState.Allow)
+			defaultRole.AddMemberPermission(Of PermissionPolicyUser)(SecurityOperations.Write, "ChangePasswordOnFirstLogon", "[Oid] = CurrentUserId()", SecurityPermissionState.Allow)
+			defaultRole.AddMemberPermission(Of PermissionPolicyUser)(SecurityOperations.Write, "StoredPassword", "[Oid] = CurrentUserId()", SecurityPermissionState.Allow)
+			defaultRole.AddTypePermissionsRecursively(Of PermissionPolicyRole)(SecurityOperations.Read, SecurityPermissionState.Deny)
+			defaultRole.AddTypePermissionsRecursively(Of ModelDifference)(SecurityOperations.ReadWriteAccess, SecurityPermissionState.Allow)
+			defaultRole.AddTypePermissionsRecursively(Of ModelDifferenceAspect)(SecurityOperations.ReadWriteAccess, SecurityPermissionState.Allow)
+			defaultRole.AddTypePermissionsRecursively(Of ModelDifference)(SecurityOperations.Create, SecurityPermissionState.Allow)
+			defaultRole.AddTypePermissionsRecursively(Of ModelDifferenceAspect)(SecurityOperations.Create, SecurityPermissionState.Allow)
             End If
             Return defaultRole
         End Function
