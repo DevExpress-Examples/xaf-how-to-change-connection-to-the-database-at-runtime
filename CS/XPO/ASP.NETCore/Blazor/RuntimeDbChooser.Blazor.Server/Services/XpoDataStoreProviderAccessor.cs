b11279805092a662eb5ec7fad089ce8e6b63062d
Crimp <dmitry.minakov@devexpress.com>
2022-08-01 17:15:13 +0400

    .

--- CS/XPO/ASP.NETCore/Blazor/RuntimeDbChooser.Blazor.Server/Services/XpoDataStoreProviderAccessor.cs
+++ CS/XPO/ASP.NETCore/Blazor/RuntimeDbChooser.Blazor.Server/Services/XpoDataStoreProviderAccessor.cs
@@ -8,9 +8,7 @@
 //        return xpoDataStoreProviderDictionary.GetOrAdd(connectionString, CreateDataStoreProvider);
 
 //        IXpoDataStoreProvider CreateDataStoreProvider(string connectionString) {
-//            //TODO Minakov - the enablePoolingInConnectionString parameter should be true.
-//            //The 'false' value is required only to get database name in the Updater -> ObjectSpace.Database.Contains("DB1")
-//            return XPObjectSpaceProvider.GetDataStoreProvider(connectionString, null, false);
+//            return XPObjectSpaceProvider.GetDataStoreProvider(connectionString, null, true);
 //        }
 //    }
 //}
